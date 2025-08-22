729be3d8ff4f71f0103d580e28165689b73b012d
wegun-cloudmile <wegun.hsia@mile.cloud>
2025-08-22 10:44:38 +0800

    fix test dag

--- dags/trigger_dag_test.py
+++ dags/trigger_dag_test.py
@@ -17,6 +17,6 @@ from . import internal_unit_testing
 
 
 def test_dag_import():
-    from . import example_dag
+    from . import trigger_dag
 
-    internal_unit_testing.assert_has_valid_dag(example_dag)
+    internal_unit_testing.assert_has_valid_dag(trigger_dag)
